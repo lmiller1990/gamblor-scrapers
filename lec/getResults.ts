5eb81ba4713ff10682a54974058c5340cdaa8fdf
Lachlan Miller <lachlan.miller.1990@outlook.com>
2019-08-08 18:59:06 +1000

    Add method to get date and team names

--- lec/getResults.ts
+++ lec/getResults.ts
@@ -1,19 +1,36 @@
-import * as puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
+import moment, { Moment } from "moment";
 
 import { options, args } from "./options";
 // import { getHistoricalGames } from "./getGames";
 
-type TObjective = "blood" | "turret" | "dragon" | "baron";
+
+type TObjective = "blood" | "turret" | "dragon" | "baron" | "herald";
+
 interface IFirstObjective {
   team: "blue" | "red" | null;
   objective: TObjective;
 }
 
+interface ITeams {
+  blueTeam: string;
+  redTeam: string;
+}
+
+const getGameDate = async (page: puppeteer.Page): Promise<{ date: Moment }> => {
+  const date = await page.$eval(".map-header-date", (_el: Element) => {
+    const el = _el as HTMLDivElement
+    return el.innerText.trim();
+  });
+
+  return { date: moment(date, "M/D/YYYY") };
+};
+
 const getFirstObjective = async (
   page: puppeteer.Page,
   objective: TObjective
 ): Promise<IFirstObjective> => {
-    page.evaluate(`
+  page.evaluate(`
     Object.defineProperty(
       window, 
       'objective', { 
@@ -25,16 +42,22 @@ const getFirstObjective = async (
 
   const firstObjective = await page.$$eval<IFirstObjective>(
     "image",
-    (images: SVGImageElement[]) => {
+    (_images: Element[]) => {
+      const images = _images as SVGImageElement[]
 
-      const first = images.reduce<SVGImageElement>((acc, image) => {
+      const first = images.reduce<SVGImageElement | null>((acc, image) => {
         if (!image.href.baseVal.includes(`${objective}_`)) {
           return acc;
         }
 
         if (!acc) {
           // found the first candidate
-          return image
+
+          if (image.href.baseVal.includes(`${objective}_`)) {
+            return image;
+          }
+
+          return null
         }
 
         if (image.x.baseVal < acc.x.baseVal) {
@@ -44,14 +67,43 @@ const getFirstObjective = async (
         return acc;
       }, null);
 
+      if (!first) {
+        // throw new Error(`Element not found for ${objective}`)
+        return {
+          team: null,
+          objective
+        }
+      }
+
       return {
-        team: first ? first.href.baseVal.includes("100") ? "blue" : "red" : null,
+        team: first
+          ? first.href.baseVal.includes("100")
+            ? "blue"
+            : "red"
+          : null,
         objective
       };
     }
   );
 
   return firstObjective;
+};
+
+const getTeams = async (page: puppeteer.Page): Promise<ITeams> => {
+  const teams = await page.$$eval<ITeams>(
+    ".champion-nameplate-name",
+    (_els: Element[]) => {
+      const els = _els as HTMLDivElement[]
+      // champion names are formatted SK Sacre. So we get the first and last, since blue is always LHS and red RHS.
+      // elements 0-4 are blue, and 5-9 are red.
+      return {
+        blueTeam: els[0].innerText.split(" ")[0],
+        redTeam: els[9].innerText.split(" ")[0]
+      };
+    }
+  );
+
+  return teams;
 };
 
 const getResults = async (matchHistoryLink: string) => {
@@ -69,11 +121,13 @@ const getResults = async (matchHistoryLink: string) => {
   // const rendered = await page.waitForSelector("#graph-switcher-262-container");
   await page.waitForSelector(".team-100-kills-bg");
 
-  const objective = "dragon"
+  const objectiveToScrape = "herald";
 
-  const result = await getFirstObjective(page, objective);
+  const { blueTeam, redTeam } = await getTeams(page);
+  const { objective, team } = await getFirstObjective(page, objectiveToScrape);
+  const { date } = await getGameDate(page);
 
-  console.log(`first ${objective}`, result);
+  console.log(date.format("DD-MM-YYYY"));
 };
 
 (async () => {
